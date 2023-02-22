914f1a3f2769b88bb7c6c65f3c51d0ae62c7ac07
Aashish <porwalaashish9@gmail.com>
2023-02-22 21:30:48 +0530

    swap2

---
+++ swap_without_tmp.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter first number :"))
+num2=int(input("Enter second number :"))
+print("Numbers before swapping : num1 =" ,num1," num2 =",num2)
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("Numbers after swapping : num1 =" ,num1," num2 =",num2)
