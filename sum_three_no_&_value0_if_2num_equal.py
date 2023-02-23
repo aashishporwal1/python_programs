88f394c90d368ab776dac5d9b97f8ff9ba13b663
Aashish <porwalaashish9@gmail.com>
2023-02-23 11:16:23 +0530

    value of sum will be zero if two no are equal

---
+++ sum_three_no_&_value0_if_2num_equal.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter first number :"))
+num2=int(input("Enter second number :"))
+num3=int(input("Enter third number :"))
+sum=0
+if (num1==num2 or num2==num3 or num3==num1) :
+    print("Sum is :",sum)
+else :
+    sum=num1+num2+num3
+    print("Sum is :",sum)
