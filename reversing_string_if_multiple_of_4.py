07d963f173a03c61edbd4d89967049de478128a2
Aashish <porwalaashish9@gmail.com>
2023-02-23 16:39:14 +0530

    reverse

--- reversing_string_if_multiple_of_4.py
+++ reversing_string_if_multiple_of_4.py
@@ -1,6 +1,9 @@
 str="Hello world!"
 rev=""
+print("Original string :",str)
 if(len(str)%4==0):
     for i in str[::-1]:
         rev=rev+i
-    print(rev)
+    print("Reversed string :",rev)
+else :
+    print("Length of string is not a multiple of 4.")
