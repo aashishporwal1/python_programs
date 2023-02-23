356e70b2fdeb2019be7352904d2b2c1c8b89214b
Aashish <porwalaashish9@gmail.com>
2023-02-23 18:33:18 +0530

    adding substring in middle of a string

---
+++ add_substr_in_midde.py
@@ -0,0 +1,7 @@
+str1="hello friend"
+str2="*"
+middle=len(str1)//2
+new=str1[:middle]+str2+str1[middle:]
+print(new)
+
+
--- substring_count.py
+++
@@ -1,3 +0,0 @@
-str1="Learning python programming language"
-str2="ing"
-print(str1.count(str2))
