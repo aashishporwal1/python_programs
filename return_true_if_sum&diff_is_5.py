570f8b07f991eb80d073a3e387a42bcbf3c97302
Aashish <porwalaashish9@gmail.com>
2023-02-23 11:24:36 +0530

    sum

---
+++ return_true_if_sum&diff_is_5.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter first number :"))
+num2=int(input("Enter second number :"))
+Answer=False
+if (num1==num2 or num1+num2==5 or num1-num2==5):
+    Answer=True
+    print(Answer)
+else :
+    print(Answer)
