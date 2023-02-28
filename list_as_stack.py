f49414cff619f509b08f8913d196f32437bbe914
Aashish <porwalaashish9@gmail.com>
2023-02-28 20:44:55 +0530

    List as Stack

---
+++ list_as_stack.py
@@ -0,0 +1,21 @@
+#stack follows Last-In First-Out
+List=[]
+for i in range(5):
+    List.append(i)
+    print(List)
+
+List.pop()
+print(List)
+
+List.pop()
+print(List)
+
+List.pop()
+print(List)
+
+List.pop()
+print(List)
+
+    
+
+
