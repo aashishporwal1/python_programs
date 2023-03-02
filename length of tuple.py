62e5206bcd033d264fb1e4e7ee6e058d9f8026ab
Aashish <porwalaashish9@gmail.com>
2023-03-02 21:27:33 +0530

    tuples

---
+++ length of tuple.py
@@ -0,0 +1,5 @@
+tup=(1,2,3,4,5)
+Length=0
+for i in tup:
+    Length+=1
+print("Length of the tuple is:",Length)
---
+++ removing empty tuple from list of tuples.py
@@ -0,0 +1,5 @@
+List=[('a','b','c'),('d','e'),('f','g'),()]
+for i in List:
+    if i==():
+        List.remove(i)
+print(List)
